2b544cb69ea4d4492dfda56f2200b2c4a217fc27
dedysaragih123 <118517967+dedysaragih123@users.noreply.github.com>
2023-04-14 15:31:18 +0700

    added main

---
+++ main.py
@@ -0,0 +1,16 @@
+from data import load
+import commands
+
+# Anggap semua fungsi yang dipanggil merupakan fungsi yang sudah dibuat sendiri pada modul lain
+Nmax = 105
+users = [0 for j in  range(3) for i in range(Nmax)] # Matriks data user
+candi = [0 for j in range(5) for i in range(Nmax)] # Matriks data candi
+bahan_bangunan = [0 for j in range(3) for i in range(Nmax)] # Data bahan bangunan
+
+load("file/user.csv", users)
+load("file/candi.csv", candi)
+load("file/bahan_bangunan.csv", bahan_bangunan)
+
+while True:
+  masukan = input(">>> ")
+  commands.run(masukan)
