f13f7220df026de1285777c6615b85d58410001b
Chiko but no H <franciscotrianto@gmail.com>
2023-04-14 17:48:55 +0700

    komen penjelas dikit

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ users = [0 for j in  range(3) for i in range(Nmax)] # Matriks data user
 candi = [0 for j in range(5) for i in range(Nmax)] # Matriks data candi
 bahan_bangunan = [0 for j in range(3) for i in range(Nmax)] # Data bahan bangunan
 """
-user: list[list[list[str]],int,int] = load("src/user.csv")
-candi: list[list[list[str]],int,int] = load("src/candi.csv")
-bahan_bangungan: list[list[list[str]],int,int] = load("src/bahan_bangunan.csv")
+user: list[list[list[str]],int,int] = load("src/user.csv")                      # [data,n_baris,n_kolom]
+candi: list[list[list[str]],int,int] = load("src/candi.csv")                    # n_baris adalah jumlah efektif baris pada matriks data
+bahan_bangungan: list[list[list[str]],int,int] = load("src/bahan_bangunan.csv") # n_kolom adalah jumlah efektif kolom pada matriks data
 
 while True:
   masukan = input(">>> ")
